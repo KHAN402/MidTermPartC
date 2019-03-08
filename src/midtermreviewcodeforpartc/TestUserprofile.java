fd10f0db07e191ba0e1930a1466f5b7df80181b7
KHAN402 <khan402@sheridancollege.ca>
2019-03-08 12:23:37 -0500

    this is for the midterm!

---
+++ src/midtermreviewcodeforpartc/TestUserprofile.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermreviewcodeforpartc;
+
+/**
+ *
+ * @author mahme
+ */
+public class TestUserprofile {
+    private String username;
+    private String genre;
+
+    public TestUserprofile(){
+        
+    }
+    
+    public TestUserprofile(String username, String genre) {
+        this.username = username;
+        this.genre = genre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    
+    
+    
+}
